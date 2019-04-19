4dab313e35ae3ab9d6511e32dab0b25cbb3f3623
TheVexedGerman <jmohr11@gmail.com>
2019-04-19 22:48:33 +0200

    Cleanup.

--- get_source.py
+++ get_source.py
@@ -164,68 +164,14 @@ def create_link_dictionary(soup):
 			continue
 
 		# print(title)
-
-	# if results[0].get('id') is None:
-	# 	image_url = results[0].table.tr.td.div.a.img.get('src')
-	# 	# print(image_url)
-	# 	gallery_number = re.search(r'(?<=\/nhentai/)\d+', image_url)
-	# 	if gallery_number:
-	# 		dic.update({'gallery_number': gallery_number.group(0)})
-	# 	page_number = re.search(r'(?<=\/)\d+(?=\.jpg)', image_url)
-	# 	if page_number:
-	# 		dic.update({'page_number': page_number.group(0)})
-
-	# 	title = results[0].table.tr.find('div', class_='resulttitle').strong.text
-	# 	if title and title != 'Creator: ':
-	# 		dic.update({'Title': title})
-	# 	creator = results[0].table.tr.find('div', class_='resultcontentcolumn')
-	# 	creator = re.search(r'(?<=Creator\(s\): <\/strong>).*?(?=<br\/>)', str(creator))
-	# 	if creator:
-	# 		dic.update({'Creator': creator.group(0)})
-
-	# creator = re.search(r"Creator: <\/strong>([\w\d\s\-_.*()\[\]]*)<br\/>", str(soup))
-	# if creator and dic.get('Creator') == None:
-	# 	dic.update({'Creator': creator.group(1)})
-	# material = re.search(r"Material: <\/strong>([\w\d\s\-_.*()\[\]]*)<br\/>", str(soup))
-	# if material and dic.get('Material') == None:
-	# 	dic.update({'Material': material.group(1)})
-	# author = re.search(r'Author: <\/strong><[\w\s\d="\-_\.\/\?:]*>([\w\d\s\-_.*()\[\]]*)<\/a>', str(soup))
-	# if author and dic.get('Author') == None:
-	# 	dic.update({'Author': author.group(1)})
-	# member = re.search(r'Member: <\/strong><[\w\s\d="\-_\.\/\?:]*>([\w\d\s\-_.*()\[\]]*)<\/a>', str(soup))
-	# if member and dic.get('Member') == None:
-	# 	dic.update({'Member': member.group(1)})
-
-	# for link in soup.find_all('a'):
-	# 	pg = link.get('href')
-	# 	if re.search(r"[\w]+\.deviantart\.com", pg) and dic.get('DeviantArt_art') == None:
-	# 		dic.update({'DeviantArt_art': pg})
-	# 	elif re.search(r"deviantart\.com\/view\/", pg) and dic.get('DeviantArt_src') == None:
-	# 		dic.update({'DeviantArt_src': pg})
-	# 	elif re.search(r"pixiv\.net\/member\.", pg) and dic.get('Pixiv_art') == None:
-	# 		dic.update({'Pixiv_art': pg})
-	# 	elif re.search(r"pixiv\.net\/member_illust", pg) and dic.get('Pixiv_src') == None:
-	# 		dic.update({'Pixiv_src': pg})
-	# 	elif re.search(r"gelbooru\.com\/index\.php\?page", pg) and dic.get('Gelbooru') == None:
-	# 		dic.update({'Gelbooru': pg})
-	# 	elif re.search(r"danbooru\.donmai\.us\/post\/", pg) and dic.get('Danbooru') == None:
-	# 		dic.update({'Danbooru': pg})
-	# 	elif re.search(r"chan\.sankakucomplex\.com\/post", pg) and dic.get('Sankaku') == None:
-	# 		dic.update({'Sankaku': pg})
-
 	return dic
 
 
 def get_source_data(picture_url):
 	print(picture_url)
 	resp = requests.get('http://saucenao.com/search.php?db=999&url='+picture_url)
-	# txt = resp.text.split('Low similarity results')[0] # Get rid of the low similarity results
-	# soup = BeautifulSoup(txt, 'html.parser')
-	# Needs to be parsed as xml since html parser adds inconvenient closing tags
+	# Needs to be parsed as xml since html parser adds inconvenient closing tags (pip install lxml)
 	soup = BeautifulSoup(resp.content, features='xml')
-	# with open("response.html", "a", encoding="utf-8") as f:
-	# 	f.write(f"{soup}\n")
-	# print(soup)
 	dic = create_link_dictionary(soup)
 	dic.update({'SauceNAO': 'http://saucenao.com/search.php?db=999&url='+picture_url})
 	
@@ -241,28 +187,6 @@ if __name__ == "__main__":
 	# sauces = ["https://i.imgur.com/GH0Dofm.jpg", "https://i.imgur.com/h3VhC7x.jpg", "https://i.imgur.com/LKxb5tS.png", "https://i.imgur.com/i9rH5bq.jpg", "https://i.imgur.com/er8mMZj.jpg"]
 	# Crashes
 	sauces = ["https://img2.gelbooru.com//images/7b/9f/7b9f93b720c8f4e559400d3100ad4c58.gif", "https://i.redd.it/p4oyfybedwr21.jpg", "https://i.imgur.com/MDKuBSQ.mp4", "https://i.imgur.com/MDKuBSQ.gif", "https://i.redd.it/nxlbtrgqvyq21.jpg"]
-	# sauces = ["https://giant.gfycat.com/EnragedGeneralCurassow.gif"]
-	# sauces = ["https://i.imgur.com/DHbGpl1.jpg"]
 	sauces = ["https://i.imgur.com/Z13SC8H.png"]
 	for sauce in sauces:
 		print(get_source_data(sauce))
-
-	# with open('comm', 'w') as ot:
-	# 	for line in build_comment(sauce):
-	# 		ot.write(line)
-
-	# print("\n\n")
-	# print("Overwiev:\n")
-	# print("Creator: "+sauce.get('Creator', ''))
-	# print("Material: "+sauce.get('Material', ''))
-	# print("Author: "+sauce.get('Author', ''))
-	# print("Member: "+sauce.get('Member', ''))
-	# print("\nLinks:\n")
-	# print("DeviantArt_art: "+sauce.get('DeviantArt_art', ''))
-	# print("DeviantArt_src: "+sauce.get('DeviantArt_src', ''))
-	# print("Pixiv_art: "+sauce.get('Pixiv_art', ''))
-	# print("Pixiv_src: "+sauce.get('Pixiv_src', ''))
-	# print("Gelbooru: "+sauce.get('Gelbooru', ''))
-	# print("Danbooru: "+sauce.get('Danbooru', ''))
-	# print("Sankaku: "+sauce.get('Sankaku', ''))
-	# print("\n\n")
